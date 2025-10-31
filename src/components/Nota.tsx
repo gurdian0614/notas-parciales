f1830281d6737165f188c20dd4d62cfaef06e9c2
gurdian0614 <ivan.gurdian@outlook.com>
2025-10-30 20:53:37 -0600

    Create component

---
+++ src/components/Nota.tsx
@@ -0,0 +1,15 @@
+
+const Nota: React.FC = () => {
+
+    return (
+        <div className="min-h-screen bg-gray-50 p-6 sm:p-10 font-sans">
+            <div className="max-w-4xl mx-auto">
+                <h1 className="text-3xl sm:text-5xl font-bold text-gray-900 text-center">
+                    Calculadora de Notas Parciales
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default Nota;
