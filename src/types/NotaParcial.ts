bdfbee047cfddc4f5d9d6e8ffa677c611f4452be
gurdian0614 <ivan.gurdian@outlook.com>
2025-11-04 21:02:26 -0600

    Cfreate type

---
+++ src/types/NotaParcial.ts
@@ -0,0 +1,10 @@
+
+export interface Parciales {
+    parcial1: number | "";
+    parcial2: number | "";
+    parcial3: number | "";
+    total: number;
+}
+
+
+export type NombreParcial = keyof Omit<Parciales, "total">;
